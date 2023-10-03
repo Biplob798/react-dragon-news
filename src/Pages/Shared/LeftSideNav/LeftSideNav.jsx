cfcf0d0b71d5a83034f19639f5bed22926f7c9f5
Biplob798 <mdbiplop798@gmail.com>
2023-10-03 23:11:40 +0600

    create rightsidebar

--- src/Pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,5 +1,32 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 const LeftSideNav = () => {
-  return <div className="text-2xl">Left Side</div>;
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("categories.json")
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
+  }, []);
+
+  return (
+    <div className="space-y-6">
+      <h2 className="text-2xl">All Categories</h2>
+      <p className="text-2xl w-full bg-[#E7E7E7] text-black p-4 text-center">
+        National News
+      </p>
+      {categories.map((category) => (
+        <Link
+          to={`/category/${category.id}`}
+          className="block ml-4 font-semibold text-xl text-[#E7E7E7]"
+          key={category.id}
+        >
+          {category.name}
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 export default LeftSideNav;
--- src/Pages/Shared/RightSideNav/RightSideNav.jsx
+++ src/Pages/Shared/RightSideNav/RightSideNav.jsx
@@ -1,5 +1,72 @@
+import {
+  FaFacebook,
+  FaGithub,
+  FaGoogle,
+  FaInstagram,
+  FaTwitter,
+} from "react-icons/fa";
+
+import qZone1 from "../../../assets/qZone1.png";
+import qZone2 from "../../../assets/qZone2.png";
+import qZone3 from "../../../assets/qZone3.png";
+
 const RightSideNav = () => {
-  return <div className="text-2xl">Right side</div>;
+  return (
+    <div className="border space-y-6">
+      <div className="p-4 space-y-4">
+        <h2 className="text-2xl font-bold">Login With</h2>
+        <button className="btn btn-outline w-full">
+          <FaGoogle></FaGoogle>
+          Google
+        </button>
+        <button className="btn btn-outline w-full">
+          <FaGithub></FaGithub>
+          Github
+        </button>
+      </div>
+      <div className="p-4">
+        <h2 className="text-2xl  font-bold mb-4">Find Us On</h2>
+        <a
+          className="p-4 flex items-center text-lg border rounded-t-lg"
+          href=""
+        >
+          <FaFacebook className="mr-3"></FaFacebook>
+          <span>Facebook</span>
+        </a>
+        <a className="p-4 flex items-center text-lg border-x" href="">
+          <FaTwitter className="mr-3"></FaTwitter>
+          <span>Twitter</span>
+        </a>
+        <a
+          className="p-4 flex items-center text-lg border rounded-b-lg"
+          href=""
+        >
+          <FaInstagram className="mr-3"></FaInstagram>
+          <span>Instagram</span>
+        </a>
+      </div>
+      {/* q zone  */}
+
+      <div className="p-4 space-y-4 bg-[#F3F3F3]">
+        <h2 className="text-2xl font-bold">Q Zone</h2>
+        <img src={qZone1} alt="" />
+        <img src={qZone2} alt="" />
+        <img src={qZone3} alt="" />
+      </div>
+      <div className="bg-[#05001DCC] p-4 space-y-4 text-center">
+        <h2 className="text-white text-4xl font-bold">
+          Create an Amazing Newspaper
+        </h2>
+        <p className="text-white font-normal text-sm">
+          Discover thousands of options, easy to customize layouts, one-click to
+          import demo and much more.
+        </p>
+        <button className="text-white btn btn-secondary bg-[#D72050]">
+          Learn More
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default RightSideNav;
